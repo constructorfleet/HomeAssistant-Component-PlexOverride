426f9a7f63320c02128c5e569fee5766e3ea5e89
Teagan glenn <teagan.m.glenn@gmail.com>
2023-04-28 19:24:23 -0600

    update plex media player

--- __init__.py
+++ __init__.py
@@ -20,24 +20,47 @@ CONFIG_SCHEMA = vol.Schema({
 async def async_setup(hass, config):
     """Set up the plex_override component."""
 
-    conf = config.get(DOMAIN)
+    from homeassistant.components.plex.media_player import PlexMediaPlayer
+    from homeassistant.components.plex.const import COMMON_PLAYERS, NAME_FORMAT
+    import plexapi.exceptions
 
-    from plexapi.myplex import MyPlexDevice
-    original_list_attrs = MyPlexDevice.listAttrs
+    def update(self):
+        """Refresh key device data."""
+        if not self.session:
+            self.force_idle()
+            if not self.device:
+                self._attr_available = False
+                return
 
-    def override_list_attrs(self, data, attr, rtag=None, **kwargs) -> List[str]:
-        """ Return a list of values from matching attribute. """
-        if attr != 'uri' or kwargs['etag'] != 'Connection':
-            return original_list_attrs(self, data, attr, rtag, **kwargs)
+        self._attr_available = True
 
-        client_identifier = data.attrib.get('clientIdentifier')
-        if not client_identifier or client_identifier not in conf:
-            return original_list_attrs(self, data, attr, rtag, **kwargs)
+        try:
+            if self.device.address.startswith("10.0"):
+                self.device.protocolCapabilities = "playback"
+                self.device._baseurl = f"http://{self.device.address}:32500"
+            device_url = self.device.url("/")
+        except plexapi.exceptions.BadRequest:
+            device_url = "127.0.0.1"
+        if "127.0.0.1" in device_url:
+            self.device.proxyThroughServer()
+        self._device_protocol_capabilities = self.device.protocolCapabilities
 
-        return [
-            conf.get(client_identifier)
-        ]
+        for device in filter(None, [self.device, self.session_device]):
+            self.device_make = self.device_make or device.device
+            self.device_platform = self.device_platform or device.platform
+            self.device_product = self.device_product or device.product
+            self.device_title = self.device_title or device.title
+            self.device_version = self.device_version or device.version
 
-    MyPlexDevice.listAttrs = override_list_attrs
+        name_parts = [self.device_product, self.device_title or self.device_platform]
+        if (self.device_product in COMMON_PLAYERS) and self.device_make:
+            # Add more context in name for likely duplicates
+            name_parts.append(self.device_make)
+        if self.username and self.username != self.plex_server.owner:
+            # Prepend username for shared/managed clients
+            name_parts.insert(0, self.username)
+        self._attr_name = NAME_FORMAT.format(" - ".join(name_parts))
+
+    PlexMediaPlayer.update = update
 
     return True
